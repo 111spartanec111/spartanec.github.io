dde7cd671fc1e773c96f0407444244bdace4ef2d
Alex <example@gmail.com>
2021-01-28 13:14:23 +0300

    Адаптив заработал, добавление слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -1,73 +1,16 @@
-var name="Alex";
-let number = 7;
-const pi = 3.14;
-
-number = 4;
-
-let leftBorderWidth = 200;
-
-//number
-//string строка - "", '', ``
-//true/false  правда/лож
-//null элемента нет
-//undefined без значения
-// 
-//  let obj = { 
-//  name: 'apple',
-//  color: 'green',
-//  weight:200
-//  }
-
-//Symbol
-
-//alert (1234);
-//console.log(number);
-// let answer = confirm("вам есть 18 ?");
-// console.log(answer);
-
-// let answer = prompt("вам есть 18 ?","");
-// console.log(answer);
-
-// console.log(4 + 'fdd');
-
-// let isChecked = true,
-    // isClose = false;
-// console.log(isChecked && isClose);
-// console.log(isChecked || isClose);
-
-// if (2*4 == 8*1) {
-//     console.log('Verno')
-// } else {
-//     console.log('Oshibka')
-// }
-
-// let answer = confirm("вам есть 18 ?");
-// if (answer) {
-//     console.log('Prohodite')
-// } else {
-//     console.log('Yhodite')
-// }
-
-// const num = 50;
-//  if (num<49) {
-//      console.log('nepravilno')
-//  } else if (num>100) {
-//      console.log('mnogo')
-//  } else {
-//      console.log('Verno')
-//  }
-
-// циклы:
-// for(let i = 1; i < 8; i++) {  //i = i+1 или i++
-//     console.log(i);
-// }
-
-
-// функции
-function logging(a, b) {
-    console.log(a + b)
-}
-
-logging(3 , 5);
-
-logging(6 , 5);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+        ]
+      });
+  });
