0ff42a820c782f2a5aefcde433223459febdf374
Alex <example@gmail.com>
2021-01-30 12:02:39 +0300

    Tabs no active

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,10 @@
 //       });
 //   });
 
-const slider = tns({
+
+
+
+var slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -32,3 +35,18 @@ const slider = tns({
   document.querySelector('.next').addEventListener ('click', function () {
     slider.goTo('next'); 
   });
+
+
+  (function($) {
+    $(function() {
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+});
+})(jQuery);
+
+
+
+ 
